434245e31f35ee6dcf58a6f52619a8c0cb98378d
Tolanuch <tolanuch@gmail.com>
2016-08-11 09:21:11 +0300

    Initial commit

---
+++ public/main.php
@@ -0,0 +1,15 @@
+<?php
+
+echo 'This is my fucking FRAMEWOOOORK!';
+
+define('ROOT',dirname(__FILE__).'/');
+define('IDEAL',dirname(__FILE__).'/ideal/');
+define('APP',dirname(__FILE__).'/application/');
+include IDEAL.'framework.php';
+app::gi()->start();
+
+
+
+?>
+
+<title>Framework by Tolanuch</title>
